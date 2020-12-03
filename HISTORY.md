0018343282650a20bc0050051ea7b2b6da1db76f
Jeffrey's Laptop <cjcaijie@163.com>
2020-12-03 16:16:29 +0800

    bump version, add HISTORY.md

---
+++ HISTORY.md
@@ -0,0 +1,7 @@
+## Development History for MRT
+
+### 0.0.2
+* implement mrt server, a web service is established
+
+### 0.0.1
+* implement mrt core, can run backend search
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mrt',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'Flask~=1.1.2',
